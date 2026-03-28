13d696b65b9a1ee4c650fb28dbcfd9a5aae0cd81
Angelie Iranzo <iranzo.angelieg9@gmail.com>
2026-03-29 00:32:23 +0800

    Final Test Commit
    >please disregard and refer to "UPDATE FINAL  |  03.28. Thank you!<

---
+++ eCommerceCartFunc_AppService_/CartAppService.cs
@@ -0,0 +1,42 @@
+﻿using eCommerceCartFunc_DataService_;
+using eCommerceCartFunc_Models_;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace eCommerceCartFunc_AppService_
+{
+    public class CartAppService
+    {
+        ICartDataService dataService = new cart_JSON_Data();
+
+        public CartAppService()
+        {
+            cart_JSON_Data cartJson = new cart_JSON_Data();
+        }
+
+
+        Product product = new Product()
+        {
+            ProductCode = ""
+        }; //this is to prevent errors, since i set this to required!
+        public bool addToCart(string newProductCode, int newProductQuanti)
+        {
+
+            if (dataService.productList.Count >= dataService.maxCartCount)
+            {
+                return false;
+            }
+            dataService.AddItem(newProductCode, newProductQuanti);
+            return true;
+
+
+        }
+
+        public List<Product> viewMyCart()
+        {
+            return dataService.viewCart();
+        }
+    }
+}
---
+++ eCommerceCartFunc_DataService_/CartDataService.cs
@@ -0,0 +1,26 @@
+﻿using eCommerceCartFunc_Models_;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eCommerceCartFunc_DataService_
+{
+    internal class CartDataService
+    {
+        ICartDataService dataService;
+        CartDataService(ICartDataService cartDataService)
+        {
+            dataService = cartDataService;
+        }
+
+        public void AddItem(string productInCode, int productInQuanti)
+        {
+            dataService.AddItem(productInCode, productInQuanti);
+        }
+
+        public List<Product> viewCart()
+        {
+            return dataService.viewCart();
+        }
+    }
+}
